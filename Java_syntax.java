992c7f1f37154ca006d393d1f5707619d865422e
ShotgunSurgery <adityasmagrawal@gmail.com>
2026-02-20 12:02:49 +0000

    java basic syntax

---
+++ Java_syntax.java
@@ -0,0 +1,11 @@
+public class Java_syntax{
+	// main must be made public so that JVM can access it from outside 
+	// static method means that the method belongs to the class itself and not an object
+	// JVM does not expect function to return anything (how does it handle it internally then ?)
+	public static void main(String args[]){
+		System.out.println("hello, world!");
+	}
+}
+
+// javac is the java compiler
+// .class contains java bytecode 
